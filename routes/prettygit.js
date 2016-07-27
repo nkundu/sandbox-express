16c002e0d7807ffcdbbdd59b59447b98619aca7f
Nick Kundu <nicholas.kundu@gmail.com>
2016-07-26 22:44:59 -0400

    async load the github content

--- routes/prettygit.js
+++ routes/prettygit.js
@@ -7,22 +7,28 @@ var router = express.Router();
 
 // path(*) takes everything, including slashes, in this param
 router.get('/:owner/:repo/master/:path(*)', function(req, res) {
+    res.render('prettygit', {
+        title: req.params.repo + ': ' + req.params.path,
+        code: req.params.owner + '/' 
+            + req.params.repo + '/master/' 
+            + req.params.path,
+        year: new Date().getFullYear()
+            });
+});
+
+router.get('/raw/:owner/:repo/master/:path(*)', function(req, res) {
     request('https://raw.githubusercontent.com/' 
                 + req.params.owner + '/' 
                 + req.params.repo + '/master/' 
                 + req.params.path, 
         function (error, response, html) {
             if (!error && response.statusCode == 200) {
-                // parse the element
-                res.render('prettygit', {
-                    title: req.params.repo + ': ' + req.params.path,
-                    code: html,
-                    year: new Date().getFullYear()
-                });
+                res.send(html);
             } else {
                 res.status(500).send('Error!');
             }
     });
 });
+
 
 module.exports = router;
